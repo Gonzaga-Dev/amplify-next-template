0108b8ead35f779a4dd37801faadb2f65dc29620
Ricardo Gonzaga <123505650+Gonzaga-Dev@users.noreply.github.com>
2025-05-24 15:52:36 -0300

    Update page.tsx

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,13 +1,12 @@
 "use client";
 
-import { generateClient } from "aws-amplify/data";
-import type { Schema } from "@/amplify/data/resource";
 import { useState } from "react";
 import { Amplify } from "aws-amplify";
 import outputs from "@/amplify_outputs.json";
+import { generateClient } from "aws-amplify/data";
+import type { Schema } from "@/amplify/data/resource";
 
 Amplify.configure(outputs);
-
 const client = generateClient<Schema>();
 
 export default function AdminPage() {
